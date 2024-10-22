04144418a755f6a5ad918e89bc8a4211c863d9de
Viktor Sharha <vsharha07@gmail.com>
2024-10-22 13:35:44 +0100

    migrated to ts

---
+++ built/script.js
@@ -0,0 +1,64 @@
+let player1;
+let player2;
+let p1winCounter = 0;
+let p2winCounter = 0;
+let gameStatus = "";
+const buttons = document.querySelectorAll("button");
+const p1display = document.querySelector("#p1");
+const p2display = document.querySelector("#p2");
+const statusDisplay = document.querySelector("#status");
+const p1winDisplay = document.querySelector("#wins h2");
+const p2winDisplay = document.querySelector("#losses h2");
+const style = getComputedStyle(document.body);
+const rps = ["🪨", "📄", "✂️"];
+function getCSSvar(varName) {
+    return style.getPropertyValue("--" + varName);
+}
+function onReady() {
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            processInput(this);
+        });
+    }
+}
+window.onload = onReady;
+// random
+function randint(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function rchoice(array) {
+    return array[randint(0, array.length)];
+}
+// game
+function checkWin() {
+    if (player1 == player2) {
+        gameStatus = "Draw!";
+        statusDisplay.style.background = getCSSvar("draw-color");
+        return;
+    }
+    if ((player1 == "📄" && player2 == "🪨") ||
+        (player1 == "✂️" && player2 == "📄") ||
+        (player1 == "🪨" && player2 == "✂️")) {
+        p1winCounter++;
+        statusDisplay.style.background = getCSSvar("win-color");
+        gameStatus = "You won!";
+    }
+    else {
+        p2winCounter++;
+        statusDisplay.style.background = getCSSvar("loss-color");
+        gameStatus = "You lost!";
+    }
+}
+function processInput(el) {
+    statusDisplay.style.width = getCSSvar("status-width");
+    statusDisplay.style.padding = "0.5rem";
+    document.querySelector("#players").style.display = "grid";
+    player1 = el.innerHTML;
+    player2 = rchoice(rps);
+    p1display.innerHTML = player1;
+    p2display.innerHTML = player2;
+    checkWin();
+    statusDisplay.innerHTML = gameStatus;
+    p1winDisplay.innerHTML = p1winCounter.toString();
+    p2winDisplay.innerHTML = p2winCounter.toString();
+}
--- src/script.ts
+++ src/script.ts
@@ -1,22 +1,22 @@
-let player1;
-let player2;
+let player1: string;
+let player2: string;
 
 let p1winCounter = 0;
 let p2winCounter = 0;
 let gameStatus = "";
 
-const buttons = document.querySelectorAll("button");
-const p1display = document.querySelector("#p1");
-const p2display = document.querySelector("#p2");
-const statusDisplay = document.querySelector("#status");
-const p1winDisplay = document.querySelector("#wins h2");
-const p2winDisplay = document.querySelector("#losses h2");
+const buttons: NodeListOf<HTMLElement> = document.querySelectorAll("button");
+const p1display: HTMLElement = document.querySelector("#p1");
+const p2display: HTMLElement = document.querySelector("#p2");
+const statusDisplay: HTMLElement = document.querySelector("#status");
+const p1winDisplay: HTMLElement = document.querySelector("#wins h2");
+const p2winDisplay: HTMLElement = document.querySelector("#losses h2");
 
 const style = getComputedStyle(document.body);
 
 const rps = ["🪨", "📄", "✂️"];
 
-function getCSSvar(varName) {
+function getCSSvar(varName: string) {
 	return style.getPropertyValue("--" + varName);
 }
 
@@ -28,15 +28,15 @@ function onReady() {
 	}
 }
 
-window.onload = onReady();
+window.onload = onReady;
 
 // random
 
-function randint(min, max) {
+function randint(min: number, max: number) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function rchoice(array) {
+function rchoice(array: Array<any>) {
 	return array[randint(0, array.length)];
 }
 
@@ -64,10 +64,10 @@ function checkWin() {
 	}
 }
 
-function processInput(el) {
+function processInput(el: HTMLElement) {
 	statusDisplay.style.width = getCSSvar("status-width");
 	statusDisplay.style.padding = "0.5rem";
-	document.querySelector("#players").style.display = "grid";
+	(document.querySelector("#players") as HTMLElement).style.display = "grid";
 
 	player1 = el.innerHTML;
 	player2 = rchoice(rps);
@@ -78,6 +78,6 @@ function processInput(el) {
 	checkWin();
 
 	statusDisplay.innerHTML = gameStatus;
-	p1winDisplay.innerHTML = p1winCounter;
-	p2winDisplay.innerHTML = p2winCounter;
+	p1winDisplay.innerHTML = p1winCounter.toString();
+	p2winDisplay.innerHTML = p2winCounter.toString();
 }
